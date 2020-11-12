8fbf8ac5c0c834747ef2139f4113f0724ef9c896
Edesalher <edalher117@gmail.com>
2020-11-11 18:38:53 -0600

    v6. Se agregó manejo de excepciones en el menú y encriptación de audio.

--- ClientApp/CLIENT_APP.py
+++ ClientApp/CLIENT_APP.py
@@ -30,78 +30,74 @@ def check_exit_app_flag():
 
 def menu_options():
     first_menu()
-    option = new_input('\n\tOpción > ', 1, 36)
-    check_exit_app_flag()
+    while True:
+        valid_options = ('1', '2', '3')
+        option = new_input('\n\tOpción > ', 1, 36)
+        check_exit_app_flag()
+        if option in valid_options:
+            break
+        else:
+            logging.warning('\x1b[0;31m' + 'Ups! esa opción no es válida.' + '\x1b[;m')
 
     if option == SEND_TEXT:
         second_menu()
-        destination = new_input('\n\tOpción > ', 1, 36)
-        check_exit_app_flag()
-
-        if destination == TO_USER:
-            destination_user = new_input('\tUser ID > ', 1, 36)
+        while True:
+            valid_options2 = ('a', 'b')
+            destination = new_input('\n\tOpción > ', 1, 36)
             check_exit_app_flag()
+            if destination in valid_options2:
+                break
+            else:
+                logging.warning('\x1b[0;31m' + 'Ups! esa opción no es válida.' + '\x1b[;m')
+        if destination == TO_ROOM:
+            destination_room = new_input('\tSala > ', 1, 36)
+            topic = f'salas/{GROUP}/{destination_room}'
+        else:
+            destination_user = new_input('\tUser ID > ', 1, 36)
             topic = f'usuarios/{GROUP}/{destination_user}'
 
-            text = new_input('\tEscriba su mensajes >> ', 1, 36)
-            check_exit_app_flag()
-            message = f'{text}|{myID}'
-            if encryption:
-                encrypted_message = ec.encrypt_message(message)
-                user.send_text(topic, encrypted_message)
-            else:
-                user.send_text(topic, message)
-        elif destination == TO_ROOM:
-            destination_room = new_input('\tSala > ', 1, 36)
-            check_exit_app_flag()
-            topic = f'salas/{GROUP}/{destination_room}'
-
-            text = new_input('\tEscriba su mensajes >> ', 1, 36)
-            check_exit_app_flag()
-            message = f'{text}|{myID}'
-            if encryption:
-                encrypted_message = ec.encrypt_message(message)
-                user.send_text(topic, encrypted_message)
-            else:
-                user.send_text(topic, message)
+        check_exit_app_flag()
+        text = new_input('\tEscriba su mensajes >> ', 1, 36)
+        check_exit_app_flag()
+        message = f'{text}|{myID}'
+        if encryption:
+            encrypted_message = ec.encrypt_message(message)
+            user.send_text(topic, encrypted_message)
+        else:
+            user.send_text(topic, message)
 
     elif option == SEND_VOICE_NOTE:
         second_menu()
-        destination = new_input('\n\tOpción > ', 1, 36)
+        while True:
+            valid_options2 = ('a', 'b')
+            destination = new_input('\n\tOpción > ', 1, 36)
+            check_exit_app_flag()
+            if destination in valid_options2:
+                break
+            else:
+                logging.warning('\x1b[0;31m' + 'Ups! esa opción no es válida.' + '\x1b[;m')
+        if destination == TO_ROOM:
+            destination = new_input('\tSala > ', 1, 36)
+        else:
+            destination = new_input('\tUser ID > ', 1, 36)
         check_exit_app_flag()
-
-        if destination == TO_USER:
-            destination_user = new_input('\tUser ID > ', 1, 36)
-            check_exit_app_flag()
-            while True:
-                duration = new_input('\tIngrese la duración de la nota de voz en seg. > ', 1, 36)
+        while True:
+            try:
+                duration = int(new_input('\tIngrese la duración de la nota de voz en seg. > ', 1, 36))
                 check_exit_app_flag()
-                if int(duration) > 30:
-                    print('\n')
-                    logging.error('\x1b[0;31m'+'La duración no debe ser mayor a 30 seg.\n'+'\x1b[;m')
+                if duration > 30:
+                    logging.error('\x1b[0;31m' + 'La duración no debe ser mayor a 30 seg.' + '\x1b[;m')
                 else:
                     break
-            print('\n')
-            logging.info('Grabando...')
-            voice_note = user.record_audio(duration)
-            logging.info('Listo')
-            user_commands.send_FTR(destination_user, voice_note)
-        elif destination == TO_ROOM:
-            destination_room = new_input('\tSala > ', 1, 36)
-            check_exit_app_flag()
-            while True:
-                duration = new_input('\tIngrese la duración de la nota de voz en seg. > ', 1, 36)
-                check_exit_app_flag()
-                if int(duration) > 30:
-                    print('\n')
-                    logging.error('\x1b[0;31m' + 'La duración no debe ser mayor a 30 seg.\n' + '\x1b[;m')
-                else:
-                    break
-            print('\n')
-            logging.info('Grabando...')
-            voice_note = user.record_audio(duration)
-            logging.info('Listo')
-            user_commands.send_FTR(destination_room, voice_note)
+            except ValueError:
+                logging.warning('\x1b[0;31m' + 'Ups! debes ingresar un número.' + '\x1b[;m')
+        print('\n')
+        logging.info('Grabando...')
+        voice_note = user.record_audio(duration)
+        if encryption:
+            ec.encrypt_audio(voice_note)
+        logging.info('Listo')
+        user_commands.send_FTR(destination, voice_note)
 
     elif option == EXIT:
         print('\n')
@@ -119,6 +115,6 @@ except KeyboardInterrupt:
     logging.warning('\x1b[0;31m'+'DESCONECTÁNDOSE DEL BROKER MQTT...'+'\x1b[;m')
 
 finally:
-    user.get_client().loop_stop()
-    user.get_client().disconnect()
+    user.client.loop_stop()
+    user.client.disconnect()
     logging.info('\x1b[0;31m'+'SE HA DESCONECTADO DEL BROKER. SALIENDO...'+'\x1b[;m')
